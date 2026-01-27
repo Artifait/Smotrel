94a1869d5de60693dccd3d3833efc13c225ec0a1
Artifait <am3044288@gmail.com>
2026-01-27 16:12:44 +0700

    MiniUpdate

---
+++ Smotrel/Converters/InverseConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Smotrel.Converters
+{
+    public class InverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Предполагаем, что value это double
+            return -1.0 * (double)value; // Инвертируем, если прокрутка идет "вверх"
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return -1.0 * (double)value;
+        }
+    }
+}
---
+++ Smotrel/Converters/MultiplyConverter.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Smotrel.Converters
+{
+    public sealed class MultiplyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is double v && double.TryParse(parameter.ToString(), out var k) ? v * k : System.Windows.Data.Binding.DoNothing;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => System.Windows.Data.Binding.DoNothing;
+    }
+}
