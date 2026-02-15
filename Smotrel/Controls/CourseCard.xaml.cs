2a4afdc335e7d876ace0006da0fbf5cc70233097
Artifait <am3044288@gmail.com>
2026-02-15 12:15:22 +0700

    refactor(controls): Extract material card into custom control

---
+++ Smotrel/Controls/CourseCard.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Smotrel.Controls
+{
+    public partial class CourseCard : UserControl
+    {
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", 
+                typeof(string), typeof(CourseCard),
+                new UIPropertyMetadata(String.Empty, new PropertyChangedCallback(LabelChanged)));
+
+        private static void LabelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CourseCard s = (CourseCard)d;
+            TextBlock tb = s.labelDisplay;
+            tb.Text = e.NewValue as string;
+        }
+
+        public CourseCard() => InitializeComponent();
+    }
+}
